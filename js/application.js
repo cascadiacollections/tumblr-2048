3b02d815dda6a83764802b0d90da88e7827347eb
Kevin T. Coughlin <kevin@cascadiacollections.com>
2021-06-26 19:58:57 -0700

    Refactor

--- js/application.js
+++ js/application.js
@@ -1,28 +1,33 @@
 // @ts-check
+'use strict';
+
+function getTumbles() {
+    const limit = 11;
+    const apiKey = "pzlRGPOKNPtpU74Uxd66TM2b6JIdUhKSOaJ0p7dGdIVv7IffSU";
+    const tumblrURL = `https://api.tumblr.com/v2/blog/newscatgif.tumblr.com/posts/photo?limit=${limit}&callback=?`;
+
+    // @ts-ignore
+    return $.ajax({
+        type: "GET",
+        url : tumblrURL,
+        dataType: "jsonp",
+        jsonpCallback: 'jsonpCallback',
+        cache: true,
+        data: {
+            api_key : apiKey
+        }
+    });
+}
+
+// @ts-ignore
+window.tumbles = {};
 window.requestAnimationFrame(function() {
-    window.tumbles = {};
-    var apiKey = "pzlRGPOKNPtpU74Uxd66TM2b6JIdUhKSOaJ0p7dGdIVv7IffSU";
-    var limit = 11;
-    var tumblrURL = "https://api.tumblr.com/v2/blog/newscatgif.tumblr.com/posts/photo?limit=" + limit + "&callback=?";
-
-
-    function getTumbles() {
-        return $.ajax({
-            type: "GET",
-            url : tumblrURL,
-            dataType: "jsonp",
-            jsonpCallback: 'jsonpCallback',
-            cache: true,
-            data: {
-                api_key : apiKey
-            }
-        });
-    }
-
-    getTumbles().done(function(data) {
-        $.each(data.response.posts, function(key, value) {
+    getTumbles().done(function(/** @type {{ response: { posts: any; }; }} */ data) {
+        // @ts-ignore
+        $.each(data.response.posts, function(/** @type {number} */ key, /** @type {{ photos: { original_size: { url: any; }; }[]; }} */ value) {
             var index = Math.pow(2, key + 1);
-            tumbles[index] = {
+            // @ts-ignore
+            window.tumbles[index] = {
                 id : key,
                 url: value.photos[0].original_size.url
             };
